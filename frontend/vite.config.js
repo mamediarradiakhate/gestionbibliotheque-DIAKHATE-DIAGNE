e0e0a91b698ccec57278da26305de23d6dd4673e
mamediarradiakhate <diakhate.mamediarrabousso99@gmail.com>
2026-03-23 20:07:04 +0100

    Update frontend dev port to 3255

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3255,
     proxy: {
       '/api/users': {
         target: 'http://localhost:2700',
